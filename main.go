d9d8558044030d10c4c07c011f76edcf71d93038
Jirapon <Ohmsm0150@gmail.com>
2020-04-05 23:39:39 +0700

    make it better

--- main.go
+++ main.go
@@ -28,5 +28,5 @@ func main() {
 	r.POST("/createFaculty", createFaculty)
 	r.POST("/updateFacultyById", updateFacultyById)
 
-	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run("0.0.0.0:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
