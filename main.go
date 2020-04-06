a0a674606fe6fbc3f87400b2671d43f5a598a827
Jirapon <Ohmsm0150@gmail.com>
2020-04-06 11:02:14 +0700

    fix main

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 
 func main() {
 	r := gin.New()
-  	r.Use(gin.Logger())
-	InitDB()
+	r.Use(gin.Logger())
+	//InitDB()
 
 	//
 	//r.Use(cors.New(cors.Config{
@@ -28,5 +28,5 @@ func main() {
 	r.POST("/createFaculty", createFaculty)
 	r.POST("/updateFacultyById", updateFacultyById)
 
-	r.Run("0.0.0.0:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
